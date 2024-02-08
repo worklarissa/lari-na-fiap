dd797f5a2dd8764e7cecf3b2f5e72b1a4bf6e98f
Larissa L. Oliveira <91492813+worklarissa@users.noreply.github.com>
2024-02-08 13:39:55 +0000

    inicio da aula do dia 08

---
+++ aula 08-02-24/script.js
@@ -0,0 +1,17 @@
+function move() {
+    var elemento = document.getElementById(animate);
+    var pos=0;
+     
+    function quadro(){
+        if(pos==850){
+
+        }
+        else{
+            pos++;
+            elemento.style.top =pos + "px";
+            elemento.style.top =pos + "px";
+
+        }
+    }
+
+}
