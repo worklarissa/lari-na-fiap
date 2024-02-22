a9f7fcc122b177c6da372cec34c809b03aee8e41
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-22 11:58:39 -0300

    aula220224

---
+++ aula22-02-2024/aula2002/src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+export default function App(){ //nome da função é o mesmo do arquivo
+  return (//primeiro elemente sempre é uma tag vazia ou uma div
+    <> 
+      <h1>Hello world!</h1>
+    </>
+  )
+
+}
